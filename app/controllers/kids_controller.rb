38b37f4a43edd8a78d740356cb770be5da4dc3c1
Jules <jules.jamet@gmail.com>
2020-09-25 13:20:04 +0100

    kid index / kid new controller settled/ kid destroy

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,6 +1,30 @@
 class KidsController < ApplicationController
 
     def index
-        
+        @kids = Kid.all
+    end
+
+    def new
+        @kid = Kid.new
+    end
+
+    def create
+        @kid = Kid.new(restaurant_params)
+        @kid.save
+        redirect_to kid_path(@kid)
+    end
+
+    def destroy
+        @kid = kid.find(params[:id])
+        @kid.destroy
+    
+        # no need for app/views/kids/destroy.html.erb
+        redirect_to kids_path
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:kid).permit(:name, :age, :school)
     end
 end
